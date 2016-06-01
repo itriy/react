38811ba9c4cc2728730243c25800cf2e0d5d6fa6
Roman <kaboom.ua@gmail.com>
2016-06-01 12:13:01 -0700

    Update CommentList.js

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -12,7 +12,7 @@ function CommentList(props){
 
 	const { comments } = props;
 	const commentItems = comments ? comments.map((comment) => <li key={comment.id}><div><Comment comment = {comment}/></div></li>) : null;
-
+	//Решение правильное, но такие условия тяжело читаються, разбивайте их
 	const commentsBody = isOpenComments ?  <div><h6 onClick = {toggleOpenComments}>Hide comments</h6>
 											{commentItems}</div>
                                         :   
